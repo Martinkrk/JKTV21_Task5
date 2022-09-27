6b744e6951fee6e3ec3e91bba7ec61cb1d8d56b1
Martinkrk <gerstmanmartin@gmail.com>
2022-09-27 21:16:03 +0300

    TWO LOOPS SORT

--- src/jktv21task5gerstman/JKTV21Task5Gerstman.java
+++ src/jktv21task5gerstman/JKTV21Task5Gerstman.java
@@ -1,60 +1,88 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package jktv21task5gerstman;
 
 
-import java.util.Arrays;
 import java.util.Random;
-/**
- *
- * @author pupil
- */
 public class JKTV21Task5Gerstman {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
         Random rnd = new Random();
         int[][] jagged = new int[5][];
-        int[] all = new int[32];
-        
+//        int[] all = new int[32];
+
         jagged[0] = new int[3];
         jagged[1] = new int[5];
         jagged[2] = new int[7];
         jagged[3] = new int[8];
         jagged[4] = new int[9];
-              
+
         System.out.println("Unsorted: ");
         for(int i = 0; i < jagged.length; i++){
             for(int j = 0; j < jagged[i].length; j++){
                 jagged[i][j] = rnd.nextInt(100 - 50 + 1) + 50;
                 System.out.printf(jagged[i][j] + " ");
-            }    
+            }
             System.out.println();
         }
+
+        //Sorting: The simple way
+
+//        int counter = 0;
+//        for(int i = 0; i < jagged.length; i++){
+//            for(int j = 0; j < jagged[i].length; j++){
+//                all[counter] = jagged[i][j];
+//                counter++;
+//            }
+//        }
+//        Arrays.sort(all);
+//        counter = 0;
+//
+//        for(int i = 0; i < jagged.length; i++){
+//            for(int j = 0; j < jagged[i].length; j++){
+//                jagged[i][j] = all[counter];
+//                counter++;
+//            }
+//        }
+
+        //Sorting: Using two loops
+
+        //Find num of elements
+        int sum = 0;
+        for(int[] arr : jagged){
+            sum += arr.length;
+        }
         int counter = 0;
-        
-        for(int i = 0; i < jagged.length; i++){
-            for(int j = 0; j < jagged[i].length; j++){
-                all[counter] = jagged[i][j];
-                counter++;
+        int subArrN;
+        int eP, eN;
+
+        for(int i = 0; i < sum*sum - 1; i++){
+            subArrN = jagged[0].length - 1;
+            for(int j = 0; j < sum - 1; j++){
+                eP = jagged[counter][j];
+                if(j == subArrN){
+                    if(counter + 1 == jagged.length){
+                        counter = 0;
+                        break;
+                    }
+                    counter += 1;
+                    eN = jagged[counter][0];
+                    if(eP > eN){
+                        jagged[counter-1][j] = eN;
+                        jagged[counter][0] = eP;
+                    }
+                    subArrN = jagged[counter].length - 1;
+                    j = -1;
+                }
+                else{
+                    eN = jagged[counter][j+1];
+                    if(eP > eN){
+                        jagged[counter][j] = eN;
+                        jagged[counter][j+1] = eP;
+                    }
+                }
             }
         }
-        
-        Arrays.sort(all);
-        counter = 0;
-        
-        for(int i = 0; i < jagged.length; i++){
-            for(int j = 0; j < jagged[i].length; j++){
-                jagged[i][j] = all[counter];
-                counter++;
-            }
-        }
+
+
         
         System.out.println("\nSorted: ");
         for(int i = 0; i < jagged.length; i++){
